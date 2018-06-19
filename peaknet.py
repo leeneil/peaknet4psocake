46677ab6ff4da8cba247d783aa501908002d4a95
nan <leeneil@users.noreply.github.com>
2018-06-19 11:57:01 -0700

    fix indent

--- peaknet.py
+++ peaknet.py
@@ -49,18 +49,18 @@ class peaknet():
         return res
 
         def peaknet2psana( results ):
-        nPeaks = 0
-        for u in range(len(results)):
-            nPeaks += len(results[u])
-        s = np.zeros( (nPeaks,1) )
-        r = np.zeros( (nPeaks,1) )
-        c = np.zeros( (nPeaks,1) )
-        counter = 0
-        for u in range(len(results)):
-            for v in range(len(results[u])):
-                s[counter] = u
-                r[counter] = results[u][v][1][1]
-                c[counter] = results[u][v][1][0]
-                counter += 1
-        return s, r, c
+            nPeaks = 0
+            for u in range(len(results)):
+                nPeaks += len(results[u])
+            s = np.zeros( (nPeaks,1) )
+            r = np.zeros( (nPeaks,1) )
+            c = np.zeros( (nPeaks,1) )
+            counter = 0
+            for u in range(len(results)):
+                for v in range(len(results[u])):
+                    s[counter] = u
+                    r[counter] = results[u][v][1][1]
+                    c[counter] = results[u][v][1][0]
+                    counter += 1
+            return s, r, c
         
